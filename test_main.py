345f4a295fb641c48ca08d53a176dc5c565f6ccc
Jove723 <jvmarquez723@gmail.com>
2024-09-02 09:09:34 -0500

    arreglao ese si no me haga caso

--- test_main.py
+++ test_main.py
@@ -1,9 +1,1 @@
-# Contar caracteres
-def contar_caracteres(palabra, letra):
-    cantidad_letra = palabra.count(letra)
-    return cantidad_letra
 
-palabra2 = input("Ingresa una palabra: ")
-Letra2 = input("Ingresa la letra a detectar: ")
-resultado2 = contar_caracteres(palabra2, Letra2)
-print(f"La palabra {palabra2} tiene {resultado2} {Letra2}")
