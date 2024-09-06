82111924ba46da236ac98039d3d78e25548f7b64
JeroDuque2330 <jeroduque2007@gmail.com>
2024-09-06 06:38:19 -0500

    Actualizacion Contador de Caracteres

---
+++ main.py
@@ -0,0 +1,9 @@
+palabra = "mimamamemima"
+caracteres = len(palabra)
+print(caracteres)   
+
+def contar_caracteres(letras,caracter):
+    return letras.count(caracter)
+
+print(contar_caracteres(palabra, "m"))
+
